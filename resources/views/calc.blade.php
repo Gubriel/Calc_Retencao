820fa701ce6fa5479268f3e52257a27e2a902789
Gustavo Pinto <gustavocaspin@proton.me>
2025-10-24 13:42:03 -0300

    Main page with all inputs

--- resources/views/calc.blade.php
+++ resources/views/calc.blade.php
@@ -8,20 +8,17 @@
         {{-- Bootstrap + Tailwind --}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        @vite('resources/css/app.css')
     </head>
 
-    <body class="bg-white flex items-center justify-center h-screen relative">
-
-        <div class="w-full max-w-lg p-6">
-
-            {{-- Título dinâmico --}}
-            <h1 class="text-2xl font-bold text-center mb-6 text-red-900">
-                Possibilidade de retenção
+    <body class="flex items-center justify-center min-h-screen py-10">
+        <div class="w-full max-w-3xl p-3 bg-white shadow-2xl rounded-lg">
+            <h1 class="text-3xl font-bold text-center mb-8 text-red-900">
+            Possibilidade de Retenção
             </h1>
-
-            {{-- Exibir dados do cliente --}}
-            <div class="flex items-center space-x-3 mb-4">
-                <div class="flex p-2 items-center rounded bg-gray-100 space-x-3">
+            <div class="flex flex-wrap gap-4 justify-center mb-3">
+                <!-- bloco esquerdo -->
+                <div class="flex p-4 bg-gray-100 rounded-lg space-x-3">
                     <div class="flex-colunm space-y-1">
                         <label class="block text-red-900 font-semibold">Tempo de base:</label>
                         <label class="block text-red-900 font-semibold">Qntd. de visitas:</label>
@@ -35,7 +32,8 @@
                         <input type="text" nome="cliente_nome" value="121" class="block font-semibold w-16 text-center px-2 bg-white" disabled>
                     </div>
                 </div>
-                <div class="flex p-2 items-center rounded bg-gray-100 space-x-3">
+                <!-- bloco direito -->
+                <div class="flex p-4 bg-gray-100 rounded-lg space-x-3">
                     <div class="flex-colunm space-y-1">
                         <label class="block text-red-900 font-semibold">Ticket Médio:</label>
                         <label class="block text-red-900 font-semibold">Taxa de churn:</label>
@@ -50,20 +48,60 @@
                     </div>
                 </div>
             </div>
-            {{-- <div class="flex space-x-3 mb-6">
-                <label class="block text-red-900 font-semibold">Saldo Restante</label>
-                <input type="text" nome="cliente_nome" value="16" class="w-16 px-2 rounded bg-gray-200" disabled>
+            <!-- blocos de baixo -->
+            <div class="flex flex-wrap gap-3 justify-center mb-3">
+                <!-- bloco esquerdo -->
+                <div class="flex p-4 bg-gray-100 mb-7 rounded-lg">
+                    <div class="flex-colunm mr-2 space-y-1">
+                        <label class="block text-red-900 font-semibold">LTV:</label>
+                        <label class="block text-red-900 font-semibold">Perda LTV:</label>
+                        <label class="block text-red-900 font-semibold">Retenção:</label>
+                        <label class="block text-red-900 font-semibold">Taxa de Retenção:</label>
+                        <label class="block text-red-900 font-semibold">Opção 1 - Mensalidades:</label>
+                        <label class="block text-red-900 font-semibold">Opção 2 - Movel (MESES GRATIS):</label>
+                        <label class="block text-red-900 font-semibold">Opção 3 - SVA (MESES GRATIS):</label>
+                        <label class="block text-red-900 font-semibold">Opção 4 DOWNGRADE DE TICKET:</label>
+                    </div>
+                    <div class="flex-colunm space-y-1">
+                        <input type="text" nome="cliente_nome" value="16" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="4" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="0" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="121" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="16" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="4" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="0" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="121" class="block font-semibold w-16 text-center px-2 bg-white">
+                    </div>
+                </div>
+                <!-- bloco direito -->
+                <div class="flex p-4 bg-gray-100 mb-7 rounded-lg">
+                    <div class="flex-colunm mr-2 space-y-1">
+                        <label class="block text-red-900 font-semibold">LT:</label>
+                        <label class="block text-red-900 font-semibold">LTV:</label>
+                        <label class="block text-red-900 font-semibold">Retenção:</label>
+                        <label class="block text-red-900 font-semibold">Taxa de Retenção:</label>
+                        <label class="block text-red-900 font-semibold">Opção 1:</label>
+                        <label class="block text-red-900 font-semibold">Opção 2:</label>
+                        <label class="block text-red-900 font-semibold">Opção 3:</label>
+                        <label class="block text-red-900 font-semibold">Opção 4:</label>
+                    </div>
+                    <div class="flex-colunm space-y-1">
+                        <input type="text" nome="cliente_nome" value="16" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="4" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="0" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="121" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="16" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="4" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="0" class="block font-semibold w-16 text-center px-2 bg-white">
+                        <input type="text" nome="cliente_nome" value="121" class="block font-semibold w-16 text-center px-2 bg-white">
+                    </div>
+                </div>
             </div>
-            <div class="flex space-x-3 mb-6">
-                <label class="block text-red-900 font-semibold">Expira em</label>
-                <input type="text" nome="cliente_nome" value="16" class="w-16 px-2 rounded bg-gray-200" disabled>
-            </div> --}}
             <div class="text-center">
-                <button type="submit" class="px-6 py-2 rounded-lg bg-red-500 text-white font-semibold hover:bg-red-600">
-                    Calcular
+                <button type="submit" class="px-16 py-2 rounded-lg bg-red-500 text-white font-semibold hover:bg-red-600">
+                Calcular
                 </button>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
